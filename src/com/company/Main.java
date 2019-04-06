89cb63f8839cb7d15b46de986df7db1a4cdf1680
Sevia_6706174030 <48724549+seviamega4030@users.noreply.github.com>
2019-04-06 11:51:36 +0700

    master

---
+++ src/com/company/Main.java
@@ -0,0 +1,63 @@
+package com.company;
+
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    int n = 10;
+
+            // Top
+            for (int i = n; i > 0; i--)
+                {
+                    // Stars
+                    for (int j = 0; j < i; j++)
+                        {
+                            System.out.print(" ");
+                        }
+                    // Spaces
+                    for (int j = i; j < n; j++)
+                        {
+                            System.out.print("*");
+                        }
+                    // Stars
+                    for (int j = i; j < n; j++)
+                        {
+                            System.out.print("*");
+                        }
+                    // Stars
+                    for (int j = 0; j < i; j++)
+                        {
+                            System.out.print(" ");
+                        }
+                    System.out.println();
+
+                }
+
+            // Bottom
+            for (int i = 2; i < n + 1/* Note the shift here */; i++)
+                {
+                    // Stars
+                    for (int j = 0; j < i; j++)
+                        {
+                            System.out.print(" ");
+                        }
+                    // Spaces
+                    for (int j = i; j < n; j++)
+                        {
+                            System.out.print("*");
+                        }
+                    // Spaces
+                    for (int j = i; j < n; j++)
+                        {
+                            System.out.print("*");
+                        }
+                    for (int j = 0; j < i; j++)
+                        {
+                            System.out.print(" ");
+                        }
+                    System.out.println();
+
+                }
+        }
+}
