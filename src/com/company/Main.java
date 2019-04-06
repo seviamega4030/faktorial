683da6f1de6cfc11f9871eda6dd0073a8d2d3bcb
Sevia_6706174030 <48724549+seviamega4030@users.noreply.github.com>
2019-04-06 11:45:43 +0700

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,38 +5,59 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-    int tinggi, setengahTinggi;
-    Scanner data = new Scanner(System.in);
-    System.out.print("Masukkan tinggi : ");
+    int n = 10;
 
-    tinggi = data.nextInt();
- int l = 1;
- int n = tinggi/2;
- System.out.println();
- System.out.println("output: ");
- for(int i=1;i<=n;i++){
-     for(int k=n;k>i;k--){
-         System.out.print(" ");
-     }
-     for(int j=1;j<=l;j++){
-         System.out.print("*");
-     }
-         l+=2;
-         System.out.println("");
- }
- l -= 4;
- for(int i=(n-1);i>=1;i--){
-     for(int k=i;k<=(n-1);k++){
-         System.out.print(" ");
-     }
-     for(int j=l;j>=1;j--){
-         System.out.print("*");
-     }
-     l-=2;
-     System.out.println("");
- }
- }
+            // Top
+            for (int i = n; i > 0; i--)
+                {
+                    // Stars
+                    for (int j = 0; j < i; j++)
+                        {
+                            System.out.print("0");
+                        }
+                    // Spaces
+                    for (int j = i; j < n; j++)
+                        {
+                            System.out.print("*");
+                        }
+                    // Stars
+                    for (int j = i; j < n; j++)
+                        {
+                            System.out.print("*");
+                        }
+                    // Stars
+                    for (int j = 0; j < i; j++)
+                        {
+                            System.out.print("0");
+                        }
+                    System.out.println();
+
+                }
+
+            // Bottom
+            for (int i = 2; i < n + 1/* Note the shift here */; i++)
+                {
+                    // Stars
+                    for (int j = 0; j < i; j++)
+                        {
+                            System.out.print("0");
+                        }
+                    // Spaces
+                    for (int j = i; j < n; j++)
+                        {
+                            System.out.print("*");
+                        }
+                    // Spaces
+                    for (int j = i; j < n; j++)
+                        {
+                            System.out.print("*");
+                        }
+                    for (int j = 0; j < i; j++)
+                        {
+                            System.out.print("0");
+                        }
+                    System.out.println();
+
+                }
+        }
 }
-
-
-
